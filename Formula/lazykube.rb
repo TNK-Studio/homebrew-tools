377115755e315f05f8d4ddb06427f40dfde34382
Elf <me@elfgzp.cn>
2021-09-30 15:54:16 +0800

    Bump TNK-Studio/lazykube formula

--- Formula/lazykube.rb
+++ Formula/lazykube.rb
@@ -1,8 +1,8 @@
 class Lazykube < Formula
-  version '0.10.1'
+  version '0.10.2'
   homepage 'https://github.com/TNK-Studio/lazykube'
-  url "https://github.com/TNK-Studio/lazykube/releases/download/v0.10.1/lazykube_darwin_amd64.zip"
-  sha256 '7a820857e59f4475d27accb028842809bea498e41449cc3f0f78ca01923fa872'
+  url "https://github.com/TNK-Studio/lazykube/releases/download/v0.10.2/lazykube_darwin_amd64.zip"
+  sha256 'ccf133cebc92f0349ff0eaa32739113583677d6f24976931bb81b74e68ca3a65'
   head 'https://github.com/TNK-Studio/lazykube.git'
 
   head do
